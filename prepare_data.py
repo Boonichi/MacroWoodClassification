5cf67110059e9c11f9d66651bf784c12d8a8083c
Boonichi <pvhung1302@gmail.com>
2023-05-02 19:48:42 +0700

    Change train/valid to train/valid/test

--- prepare_data.py
+++ prepare_data.py
@@ -4,21 +4,15 @@ from pathlib import Path
 import argparse
 import shutil
 
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, train_test_split
 
 from cv2 import imread
 
 DATA_DIR = "./wood_dataset/"
 OUTPUT_DIR = "./dataset/"
-NUM_FOLDS = 5
+NUM_FOLDS = 4
 SEED = 43
 
-def Calc_mean_std(df):
-    for index, row in df.iterrows():
-        image = imread(row["imgpath"])
-        print(image)
-    
-    #return mean, std
 def prepare_data():
     df = []
 
@@ -28,32 +22,38 @@ def prepare_data():
             label, wood_name = folder.split("-")
 
             for image in os.listdir(os.path.join(DATA_DIR, folder)):
-                if (image.endswith(""))
-                imgpath = os.path.join(DATA_DIR, folder, image)
-                sample = {}
-                sample["imgpath"] = imgpath
-                sample["label"] = label
-                sample["wood_name"] = wood_name
+                if (image.endswith("")):
+                    imgpath = os.path.join(DATA_DIR, folder, image)
+                    sample = {}
+                    sample["imgpath"] = imgpath
+                    sample["label"] = label
+                    sample["wood_name"] = wood_name
                 
-                df.append(sample)
+                    df.append(sample)
 
     df = pd.DataFrame(df)
-    #df.to_csv(os.path.join(OUTPUT_DIR, "dataset.csv"))
 
+    # Split train/set
+    X_train, X_test, Y_train, Y_test = train_test_split(df,df["label"], stratify=df["label"], test_size = 0.2)
 
-    # Stratifiled K Folds (5 folds)
-    skf = StratifiedKFold(n_splits = NUM_FOLDS, shuffle= True, random_state=SEED)
-    df["fold"] = -1
-
-    for i, (train_index, val_index) in enumerate(skf.split(df, df["label"])):
-        df.loc[val_index, "fold"] = i
-
-    print(df.groupby(['fold', 'label'])['imgpath'].size())
-
-    df.to_csv(os.path.join(OUTPUT_DIR, "dataset.csv"))
+    df_train = X_train.reset_index()
+    df_test = X_test.reset_index()
     
-    mean, std = Calc_mean_std(df)
-    print("Mean {} and standard deviation {} of dataset".format(mean, std))
+    # Stratifiled K Folds (4 folds)
+    skf = StratifiedKFold(n_splits = NUM_FOLDS, shuffle= True, random_state=SEED)
+    df_train["fold"] = -1
+
+    for i, (train_index, val_index) in enumerate(skf.split(df_train, df_train["label"])):
+        df_train.loc[val_index, "fold"] = i
+
+    print(df_train.groupby(['fold', 'label'])['imgpath'].size())
+
+    df_train.to_csv(os.path.join(OUTPUT_DIR, "train_dataset.csv"))
+    df_test.to_csv(os.path.join(OUTPUT_DIR, "test_dataset.csv"))
+    
+    print(len(df_train))
+
+    # Create Train/Test Folds
     for fold in range(NUM_FOLDS):
         fold_path = f"{OUTPUT_DIR}fold_{fold}/"
         train_path = f"{fold_path}train/"
@@ -63,8 +63,8 @@ def prepare_data():
         os.makedirs(train_path, exist_ok= True)
         os.makedirs(val_path, exist_ok= True)
 
-        train_df = df[df["fold"] != fold].reset_index(drop = True)
-        val_df = df[df["fold"] == fold].reset_index(drop = True)
+        train_df = df_train[df_train["fold"] != fold].reset_index(drop = True)
+        val_df = df_train[df_train["fold"] == fold].reset_index(drop = True)
 
         for index, row in train_df.iterrows():   
             src_dir = row["imgpath"]
@@ -85,7 +85,6 @@ def prepare_data():
             os.makedirs(destination_dir, exist_ok=True)
             
             shutil.copy(src_dir, destination_dir)
-
 if __name__ == "__main__":
     if OUTPUT_DIR:
         Path(OUTPUT_DIR).mkdir(parents=True, exist_ok=True)
